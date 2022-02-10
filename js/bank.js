9780bc67ac5e73621047d7508b0145b7b4f65128
nizihad824 <mdnurulislam97@gmail.com>
2022-02-10 21:14:51 +0100

    banking page create

--- js/bank.js
+++ js/bank.js
@@ -1,13 +1,16 @@
-document.getElementById('login-submit').addEventListener('click',function(){
-   const emailField = document.getElementById('user-mail');
-   const userEmail = emailField.value;
-   console.log(userEmail)
-//    get password
 
-const passwordField =document.getElementById('user-password');
-const userPassword = passwordField.value;
 
-if(userEmail == 'sontan@baap.com'&& userPassword == 'secret'){
-    console.log('valid user')
-}
+document.getElementById('login-submit').addEventListener('click', function () {
+    const emailField = document.getElementById('user-mail');
+    const userEmail = emailField.value;
+  
+    //    get password
+
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+
+    if (userEmail == 'sontan@baap.com' && userPassword == 'secret') {
+        console.log('valid user');
+        window.location.href ='banking.html'
+    }
 })
