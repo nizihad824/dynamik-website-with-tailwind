b4ec92b7b039936c208d5631e5fe7be06e6b261d
nizihad824 <mdnurulislam97@gmail.com>
2022-02-10 19:57:00 +0100

    DOM start

---
+++ js/bank.js
@@ -0,0 +1,5 @@
+document.getElementById('login-submit').addEventListener('click',function(){
+   const emailField = document.getElementById('user-mail');
+   const userEmail = emailField.value;
+   console.log(userEmail)
+})
