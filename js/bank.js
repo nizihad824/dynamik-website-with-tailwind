e6aa23b96efcc728ca835063401f9c3916d3a965
nizihad824 <mdnurulislam97@gmail.com>
2022-02-10 21:00:14 +0100

    dom element

--- js/bank.js
+++ js/bank.js
@@ -2,4 +2,12 @@ document.getElementById('login-submit').addEventListener('click',function(){
    const emailField = document.getElementById('user-mail');
    const userEmail = emailField.value;
    console.log(userEmail)
+//    get password
+
+const passwordField =document.getElementById('user-password');
+const userPassword = passwordField.value;
+
+if(userEmail == 'sontan@baap.com'&& userPassword == 'secret'){
+    console.log('valid user')
+}
 })
