0cfad90bcd3edca36e3ff90b5831e1355a405585
nizihad824 <mdnurulislam97@gmail.com>
2022-02-14 00:20:59 +0100

    deposit amount calculation

--- js/banking.js
+++ js/banking.js
@@ -1,4 +1,20 @@
 // handle deposit button event
 document.getElementById('deposit-button').addEventListener('click',function(){
-    console.log('deposit button clicked')
+    // console.log('deposit button clicked')
+    // get amount deposited
+    const depositInput = document.getElementById('deposit-input');
+
+    const newDepositAmountText = depositInput.value;
+    const newDepositAmount=parseFloat(newDepositAmountText)
+    
+    const depositTotal = document.getElementById('deposit-total');
+    const previousDepositText = depositTotal.innerText;
+    const previousDepositAmount =parseFloat(previousDepositText);
+    const newDepositTotal =previousDepositAmount + newDepositAmount;
+    // console.log(depositTotal.innerText);
+    depositTotal.innerText = newDepositTotal;
+
+    // clear the deposit input tag
+    depositInput.value ='';
+
 })
