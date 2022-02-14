5fd072918f54e0510db87cd365f548d87e572630
nizihad824 <mdnurulislam97@gmail.com>
2022-02-14 01:16:02 +0100

    total balance update

--- js/banking.js
+++ js/banking.js
@@ -7,12 +7,28 @@ document.getElementById('deposit-button').addEventListener('click',function(){
     const newDepositAmountText = depositInput.value;
     const newDepositAmount=parseFloat(newDepositAmountText)
     
+    // update deposit total
     const depositTotal = document.getElementById('deposit-total');
+    console.log(depositTotal)
     const previousDepositText = depositTotal.innerText;
     const previousDepositAmount =parseFloat(previousDepositText);
     const newDepositTotal =previousDepositAmount + newDepositAmount;
     // console.log(depositTotal.innerText);
     depositTotal.innerText = newDepositTotal;
+
+
+    // update account balance
+    const balanceTotal =document.getElementById('balance-total');
+    console.log(balanceTotal)
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal =parseFloat(balanceTotal) ;
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+
+
+
+
 
     // clear the deposit input tag
     depositInput.value ='';
